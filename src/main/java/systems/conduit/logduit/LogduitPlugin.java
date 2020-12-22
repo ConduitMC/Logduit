5ff08a5cc51158f5f64637975f0ae22f351b6491
Innectic <innectic@gmail.com>
2020-12-22 00:01:30 -0800

    chore: init

---
+++ src/main/java/systems/conduit/logduit/LogduitPlugin.java
@@ -0,0 +1,20 @@
+package systems.conduit.logduit;
+
+import systems.conduit.main.core.plugin.Plugin;
+
+/**
+ * @author Innectic
+ * @since 12/21/2020
+ */
+public class LogduitPlugin extends Plugin {
+
+    @Override
+    protected void onEnable() {
+
+    }
+
+    @Override
+    protected void onDisable() {
+
+    }
+}
